59c3ec4def98285a98d1db32579d74fc067cbab9
zakooz <marcotribosklol@gmail.com>
2017-08-29 12:10:57 +0100

    universal5429: libGLES: add mali shim

---
+++ libshims/libgutils/Android.mk
@@ -0,0 +1,33 @@
+# Copyright (C) 2016 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+LOCAL_PATH := $(call my-dir)
+
+
+include $(CLEAR_VARS)
+<<<<<<< HEAD:libshims/libshim_gps/Android.mk
+=======
+LOCAL_SRC_FILES := libgutils.c
+LOCAL_SHARED_LIBRARIES := libcutils
+LOCAL_MODULE := libgutils
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+>>>>>>> efbcbf99 (libGLES: Update mali shim to Oreo):libgutils/Android.mk
+
+LOCAL_SHARED_LIBRARIES := liblog libcutils libgui libbinder libutils libsensor
+LOCAL_SRC_FILES := libshim_gps.cpp
+LOCAL_MODULE := libshim_gps
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_SHARED_LIBRARY)
