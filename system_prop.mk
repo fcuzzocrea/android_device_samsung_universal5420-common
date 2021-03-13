3b73308c970469bfa805f7a38ecd28430839351c
Francescodario Cuzzocrea <bosconovic@gmail.com>
2021-03-13 19:51:54 +0100

    [DNM]: debug hax

--- system_prop.mk
+++ system_prop.mk
@@ -58,3 +58,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0 \
     wifi.direct.interface=p2p-dev-wlan0
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.adb.secure=0 \
+    persist.service.adb.enable=1 \
+    persist.sys.usb.config=adb \
+    service.adb.root=1
